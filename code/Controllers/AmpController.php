d419abd5f438484a4ec5c91335a80d03dde52c50
Dustin Quam <quamsta@gmail.com>
2016-03-02 22:45:06 -0600

    minor tweaks to template and controller

--- code/Controllers/AmpController.php
+++ code/Controllers/AmpController.php
@@ -30,8 +30,11 @@ class AmpController extends Extension
         if(!$content) {
             return false;
         }
-        
+
+        //All AMP filters:
         $content = str_replace("<img", "<amp-img", $content);
+        $content = str_replace('src="assets/', 'src="'.Director::BaseURL().'/assets/', $content);
+        $content = str_replace('data-src', 'src', $content);
 
         return $content;
 
